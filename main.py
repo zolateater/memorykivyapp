3caef0f15b76e472c391bd643b93b1d303ab4199
Zolat Eater <zolateater@gmail.com>
2019-01-13 04:20:56 +0300

    Now it works a bit more

--- main.py
+++ main.py
@@ -62,18 +62,18 @@ class NumberConfig:
         for key in get_all_keys():
             self._data[key] = data[key]['person'] if data and key in data else ''
 
-#     def get_value_for(self, assoc):
-#         return self._data[assoc]
+    def get_value_for(self, assoc):
+        return self._data[assoc]
 
-#     def set_value_for(self, assoc, value) -> None:
-#         self._data[assoc] = value
+    def set_value_for(self, assoc, value):
+        self._data[assoc] = value
 
-#     def digit_group_filled(self, group):
-#         return all([self._data[key] for key in get_numbers_keys_for_group(group)])
+    def digit_group_filled(self, group):
+        return all([self._data[key] for key in get_numbers_keys_for_group(group)])
 
-#     @staticmethod
-#     def get_named_groups():
-#         return zip(DIGIT_GROUPS, DIGIT_GROUP_NAMES)
+    @staticmethod
+    def get_named_groups():
+        return zip(DIGIT_GROUPS, DIGIT_GROUP_NAMES)
 
 
 def read_config_from_stream(stream):
@@ -100,102 +100,102 @@ class NumberInputGroup(BoxLayout):
     pass
 
 
-# class ListScreen(Screen):
-#     def __init__(self, config: NumberConfig, **kwargs):
-#         super().__init__(**kwargs)
-#         self.config = config
-#         self.local_config = deepcopy(config) 
-#         self.current_group_id = None
-#         self.group_switch_buttons = WeakValueDictionary()
-#         self.inputs = WeakValueDictionary()
-#         self.populate_tabs()
-#         self.colorize_buttons()
-#         self.select_group('0')
-#         self.set_input_handlers()
+class ListScreen(Screen):
+    def __init__(self, config, **kwargs):
+        super().__init__(**kwargs)
+        self.config = config
+        self.local_config = deepcopy(config) 
+        self.current_group_id = None
+        self.group_switch_buttons = WeakValueDictionary()
+        self.inputs = WeakValueDictionary()
+        self.populate_tabs()
+        self.colorize_buttons()
+        self.select_group('0')
+        self.set_input_handlers()
 
-#     def get_group_switch_layout(self) -> BoxLayout:
-#         return self.ids.switch_layout
+    def get_group_switch_layout(self):
+        return self.ids.switch_layout
 
-#     def populate_tabs(self):
-#         parent = self.get_group_switch_layout()
-#         for chunk in by_chunk(self.local_config.get_named_groups(), 5):
-#             anchor = AnchorLayout()
-#             layout = BoxLayout()
-#             layout.spacing = '5dp'
-#             layout.size_hint_x = len(chunk) / 5
-#             parent.add_widget(anchor)
-#             anchor.add_widget(layout)
-#             for group, group_name in chunk:
-#                 button = ToggleButton(id=group, text=group_name)
-#                 button.bind(on_press=self.handle_group_release)
-#                 self.group_switch_buttons[group] = button
-#                 layout.add_widget(button)
+    def populate_tabs(self):
+        parent = self.get_group_switch_layout()
+        for chunk in by_chunk(self.local_config.get_named_groups(), 5):
+            anchor = AnchorLayout()
+            layout = BoxLayout()
+            layout.spacing = '5dp'
+            layout.size_hint_x = len(chunk) / 5
+            parent.add_widget(anchor)
+            anchor.add_widget(layout)
+            for group, group_name in chunk:
+                button = ToggleButton(id=group, text=group_name)
+                button.bind(on_press=self.handle_group_release)
+                self.group_switch_buttons[group] = button
+                layout.add_widget(button)
 
-#     def colorize_buttons(self):
-#         for group, button in self.group_switch_buttons.items():
-#             if self.local_config.digit_group_filled(group):
-#                 button.background_color = 0, 1, 0, 1
-#             else:
-#                 button.background_color = 1, 0, 0, 1
+    def colorize_buttons(self):
+        for group, button in self.group_switch_buttons.items():
+            if self.local_config.digit_group_filled(group):
+                button.background_color = 0, 1, 0, 1
+            else:
+                button.background_color = 1, 0, 0, 1
 
-#     def handle_group_release(self, btn: ToggleButton):
-#         self.select_group(btn.id)
+    def handle_group_release(self, btn):
+        self.select_group(btn.id)
 
-#     def select_group(self, group_id):
-#         for btn in self.group_switch_buttons.values():
-#             btn.state = 'normal'
+    def select_group(self, group_id):
+        for btn in self.group_switch_buttons.values():
+            btn.state = 'normal'
 
-#         btn: ToggleButton = self.group_switch_buttons[group_id]
-#         btn.state = 'down'
+        btn = self.group_switch_buttons[group_id]
+        btn.state = 'down'
 
-#         label_names = reversed(get_number_values_for_group(group_id))
-#         label_ids = reversed(get_numbers_keys_for_group(group_id))
+        label_names = reversed(get_number_values_for_group(group_id))
+        label_ids = reversed(get_numbers_keys_for_group(group_id))
 
-#         self.unset_input_handlers()
-#         for text, label_id, number_input in zip(label_names, label_ids, self.number_inputs()):
-#             number_input.ids.label.text = text
-#             number_input.ids.input.text = self.local_config.get_value_for(label_id)
-#             number_input.ids.input.config_id = label_id
+        self.unset_input_handlers()
+        for text, label_id, number_input in zip(label_names, label_ids, self.number_inputs()):
+            number_input.ids.label.text = text
+            number_input.ids.input.text = self.local_config.get_value_for(label_id)
+            number_input.ids.input.config_id = label_id
 
-#         self.set_input_handlers()
-#         self.current_group_id = group_id
+        self.set_input_handlers()
+        self.current_group_id = group_id
 
-#     def set_input_handlers(self):
-#         for number_input in self.number_inputs():
-#             number_input.ids.input.bind(text=self.update_text_value)
+    def set_input_handlers(self):
+        for number_input in self.number_inputs():
+            number_input.ids.input.bind(text=self.update_text_value)
 
-#     def unset_input_handlers(self):
-#         for number_input in self.number_inputs():
-#             number_input.ids.input.unbind(text=self.update_text_value)
+    def unset_input_handlers(self):
+        for number_input in self.number_inputs():
+            number_input.ids.input.unbind(text=self.update_text_value)
 
-#     def number_inputs(self):
-#         return self.ids.number_input_layout.children
+    def number_inputs(self):
+        return self.ids.number_input_layout.children
 
-#     def update_text(self, label_id, text):
-#         self.local_config.set_value_for(label_id, text)
+    def update_text(self, label_id, text):
+        self.local_config.set_value_for(label_id, text)
 
-#     def save_changes(self):
-#         for key, value in self.local_config._data.items():
-#             self.config.set_value_for(key, value)
-#         with open('memory_config.json', 'w') as file_stream:
-#             write_config_to_stream(file_stream, self.config)
-#         self._to_main_screen()
+    def save_changes(self):
+        for key, value in self.local_config._data.items():
+            self.config.set_value_for(key, value)
+        with open('memory_config.json', 'w') as file_stream:
+            write_config_to_stream(file_stream, self.config)
+        self._to_main_screen()
 
-#     def cancel(self):
-#         self.local_config = deepcopy(self.config)
-#         self._to_main_screen()
+    def cancel(self):
+        self.local_config = deepcopy(self.config)
+        self._to_main_screen()
 
-#     def update(self):
-#         self.select_group(self.current_group_id)
-#         self.colorize_buttons()
+    def update(self):
+        self.select_group(self.current_group_id)
+        self.colorize_buttons()
     
-#     def _to_main_screen(self):
-#         self.parent.transition.direction = 'right'
-#         self.parent.current = 'main'
+    def _to_main_screen(self):
+        self.parent.transition.direction = 'right'
+        self.parent.current = 'main'
 
-#     def update_text_value(self, instance, value):
-#         self.local_config.set_value_for(instance.config_id, value)
-#         self.colorize_buttons()
+    def update_text_value(self, instance, value):
+        self.local_config.set_value_for(instance.config_id, value)
+        self.colorize_buttons()
 
 
 
@@ -215,17 +215,17 @@ class MemoryApp(App):
 
     def read_config_from_disk(self):
         try:
-            with open('memory_config.json', 'r') as file_stream:
+            #with open('memory_config.json', 'r') as file_stream:
+            return NumberConfig()
                 # return read_config_from_stream(file_stream)
-                pass
         except FileNotFoundError:
             logging.info('No config file found.')
-            # return NumberConfig()
+            return NumberConfig()
+        except:
+            logging.exception("Cannot read the file")
 
     def build(self):
-        # self.number_config = self.read_config_from_disk()
-        # manager = MemoryScreenManager()
-        # manager.current_screen = 'main'
+        self.number_config = self.read_config_from_disk()
         return MemoryScreenManager()
 
 
@@ -237,5 +237,10 @@ No need to select current screen
 Commenting out ListScreen doesn't fix the app
 Uncommenting config code crushes the app
 Uncommenting NumberConfig and functions breaks the app
-These might be the typings
+typings crashes the app
+uncommented read config file - crashes
+commented # self.number_config = self.read_config_from_disk() app works
+seems like self.read_config_from_disk() breaks the app
+except on file read doesn't help
+seems like open('memory_config.json', 'r') as file_stream breaks the app
 """
